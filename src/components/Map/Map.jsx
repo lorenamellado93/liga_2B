9c495a2220c9f2745563fbc1890f685f6c46521b
lorenamellado93 <lorenamelladovalle@gmail.com>
2021-03-02 16:07:51 +0100

    map done w/ colors

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,103 +1,103 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import {
   ComposableMap,
   ZoomableGroup,
   Geographies,
-  Geography,
+  Geography
 } from "react-simple-maps";
-
-import './Map.scss';
 
 const wrapperStyles = {
   width: "100%",
-  maxWidth: 980,
-  margin: "0 auto",
+  maxWidth: 2000,
+  margin: "0 auto"
 };
 
-const Map = () => {
-  const [highlighted, sethighlighted] = useState("");
-  const [hovered, sethovered] = useState(false);
-
-  const MAP_URL =
-    "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/spain/spain-comunidad-with-canary-islands.json";
-
-  const handleMove = (geo) => {
-    if (hovered) return;
-    sethovered(true) && sethighlighted(geo.properties.NAME);
-    console.log(geo);
+class WorldMap extends Component {
+  state = {
+    highlighted: "",
+    hovered: false
   };
-
-  const handleLeave = () => {
-    sethighlighted("") && sethovered(false);
+  handleMove = (geo) => {
+    if (this.state.hovered) return;
+    this.setState({
+      hovered: true,
+      highlighted: geo.properties.GROUP
+    });
+    console.log(geo.properties.GROUP);
   };
+  handleLeave = () => {
+    this.setState({
+      highlighted: "",
+      hovered: false
+    });
+  };
+  render() {
+    return (
+      <div style={wrapperStyles}>
+        <ComposableMap
+          projectionConfig={{
+            scale: 1500,
+            rotation: [0, 0, 0]
+          }}
+          width={960}
+          height={551}
+          style={{
+            width: "100%",
+            height: "auto"
+          }}
+        >
+          <ZoomableGroup center={[5, 35]} disablePanning>
+            <Geographies
+              geography="https://raw.githubusercontent.com/lorenamellado93/group-spain-map/main/group-map.json"
+              disableOptimization
+            >
+              {(geographies, projection) =>
+                geographies.map((geography, i) => (
+                  <Geography
+                    key={i}
+                    cacheId={geography.properties.ISO_A3 + i}
+                    geography={geography}
+                    projection={projection}
+                    onMouseMove={this.handleMove}
+                    onMouseLeave={this.handleLeave}
+                    style={{
+                      default: {
+                        fill:
+                          geography.properties.GROUP === this.state.highlighted
+                            ? "#F50025"
+                            : "#EDEDED",
+                        stroke:
+                          geography.properties.GROUP === this.state.highlighted
+                            ? "#F50025"
+                            : "#CCCCCC",
+                        strokeWidth: 0.75,
+                        outline: "none",
+                        transition: "all 250ms"
+                      },
+                      hover: {
+                        fill: "#F50025",
+                        stroke: "#F50025",
+                        strokeWidth: 0.75,
+                        outline: "none",
+                        transition: "all 250ms"
+                      },
+                      pressed: {
+                        fill: "#DD4132",
+                        stroke: "#9E1030",
+                        strokeWidth: 0.75,
+                        outline: "none",
+                        transition: "all 250ms"
+                      }
+                    }}
+                  />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    );
+  }
+}
 
-  console.log(highlighted);
-
-  return (
-    <div >
-    <div style={wrapperStyles}>
-      <ComposableMap
-        projectionConfig={{
-          scale: 800,
-
-        }}
-        width={980}
-        height={551}
-        style={{
-          width: "200%",
-          height: "auto",
-        }}
-      >
-        <ZoomableGroup center={[0, 0]} disablePanning>
-          <Geographies geography={MAP_URL}>
-            {({ geographies, projection}) =>
-              geographies.map((geo, i) => (
-                <Geography 
-                key={i}
-                cacheId={geo.properties.ISO + i}
-                geography={geo}
-                projection={projection}
-                onMouseMove={handleMove}
-                onMouseLeave={handleLeave}
-                style={{
-                    default: {
-                      fill:
-                      geo.properties.NAME ===
-                        highlighted
-                          ? "#DD4132"
-                          : "#F0EAD6",
-                      stroke:
-                      geo.properties.NAME ===
-                        highlighted
-                          ? "#9E1030"
-                          : "#B2A27D",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                      transition: "all 250ms"
-                    },
-                    hover: {
-                      fill: "#FF6F61",
-                      stroke: "#9E1030",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                      transition: "all 250ms"
-                    },
-                    pressed: {
-                      fill: "#DD4132",
-                      stroke: "#9E1030",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                      transition: "all 250ms"
-                    }
-                  }}/>
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </div>
-    </div>
-  );
-};
-
-export default Map;
+export default WorldMap;
